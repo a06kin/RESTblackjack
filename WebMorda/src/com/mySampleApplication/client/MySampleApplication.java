c760290581c3181b84e1ab62c04e322d616a9a21
aaa <a06kin@gmail.com>
2014-01-27 18:21:39 +0200

    new html style

--- WebMorda/src/com/mySampleApplication/client/MySampleApplication.java
+++ WebMorda/src/com/mySampleApplication/client/MySampleApplication.java
@@ -16,45 +16,70 @@ public class MySampleApplication implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        final Button button = new Button("Click me");
-        final Label label = new Label();
-        final DockPanel dPanel = new DockPanel();
-        final TextArea tArea = new TextArea();
+        final Button bRefreshAll = new Button("RefreshAll");
+        final Button bNextStep = new Button("NextStep");
+        final TextArea tNow = new TextArea();
+        final TextArea tNextStep = new TextArea();
 
-        button.addClickHandler(new ClickHandler() {
+        bRefreshAll.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                if (label.getText().equals("")) {
-                    MySampleApplicationService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
-                } else {
-                    label.setText("");
-                }
+                MySampleApplicationService.App.getInstance().getMessage("Hello, World!", new bRefreshAllClick(tNow));
             }
         });
 
+        bNextStep.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                MySampleApplicationService.App.getInstance().getMessage("Hello, World!", new bNextStepClick(tNextStep));
+            }
+        });
 
-//        tArea.setEnabled(false);
-        tArea.setReadOnly(true);
-        dPanel.add(tArea, DockPanel.CENTER);
-        dPanel.add(button, DockPanel.NORTH);
-        dPanel.add(label, DockPanel.NORTH);
+        //TA now
+        tNow.setReadOnly(true);
+        tNow.setHeight("50%");
+        tNow.setWidth("30%");
 
-        RootPanel.get("main").add(dPanel);
+        //TA nextstep
+        tNextStep.setReadOnly(true);
+        tNextStep.setHeight("50%");
+        tNextStep.setWidth("30%");
+
+
+        RootPanel.get("buttons").add(bRefreshAll);
+        RootPanel.get("buttons").add(bNextStep);
+        RootPanel.get("textareas").add(tNow);
+        RootPanel.get("textareas").add(tNextStep);
 
     }
 
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
+    private static class bRefreshAllClick implements AsyncCallback<String> {
+        private TextArea tNow;
 
-        public MyAsyncCallback(Label label) {
-            this.label = label;
+        public bRefreshAllClick(TextArea tNow) {
+            this.tNow = tNow;
         }
 
         public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
+            tNow.setText(tNow.getText() + result + "\n");
         }
 
         public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
+            tNow.setText("Failed to receive answer from server!");
+        }
+    }
+
+    private static class bNextStepClick implements AsyncCallback<String> {
+        private TextArea tNextStep;
+
+        public bNextStepClick(TextArea tNextStep) {
+            this.tNextStep = tNextStep;
+        }
+
+        public void onSuccess(String result) {
+            tNextStep.setText(tNextStep.getText() + result + "\n");
+        }
+
+        public void onFailure(Throwable throwable) {
+            tNextStep.setText("Failed to receive answer from server!");
         }
     }
 }
